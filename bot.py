d66fba59ff094e9d31b52b14f3415dec7eb45d19
Сергей Круглов <kruser@yandex.ru>
2023-11-29 12:01:19 +0500

    add README

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import sys
 import asyncio
 import logging
+import httpx
 from textwrap import dedent
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
@@ -39,7 +40,11 @@ async def get_weather(message: Message):
     else:
 
         city = message.text
-        weather = await city_weather_request(city)
+        try:
+            weather = await city_weather_request(city)
+        except httpx.ConnectError:
+            await message.reply('Сервис недоступен, попробуйте позже')
+            return
 
         if 'error' in weather.keys():
             await message.reply('Укажите корректное название города')
